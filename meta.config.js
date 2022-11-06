1b145585979bb5d9ac2f559e0d1ba78424521885
Taowuu <taowuu@foxmail.com>
2022-11-06 20:03:29 +0800

    1

--- meta.config.js
+++ meta.config.js
@@ -3,15 +3,61 @@ export default {
   skills: [
     'JavaScript',
     'TypeScript',
+    'Vue.js',
+    'Webpack',
+    'Node.js',
+    'HTTP',
+    'Data Structure',
+    'Algorithm',
+    'Machine Learning',
+    'HTML',
+    'CSS',
   ],
   blogs: [
     {
-      title: '111',
-      url: 'http://qq.com'
+      title: '前端知识沉淀',
+      url: 'https://github.com/taowuu/tao-blog'
     },
     {
-      title: '222',
-      url: 'http://qq.com'
+      title: '前端手写大集合',
+      url: 'https://github.com/taowuu/tao-write'
     },
+    {
+      title: '前端布局大集合',
+      url: 'https://github.com/taowuu/tao-layout'
+    },
+    {
+      title: '前端面试攻略大全',
+      url: 'https://github.com/taowuu/fe-interview'
+    },
+    {
+      title: 'LeetCode JS 题解',
+      url: 'https://github.com/taowuu/tao-leetcode'
+    },
+    {
+      title: '一步步实现 Vue',
+      url: 'https://github.com/taowuu/tao-vue'
+    },
+    {
+      title: '一步步实现 Vuex',
+      url: 'https://github.com/taowuu/tao-vuex'
+    },
+    {
+      title: '一步步实现 Vue Router',
+      url: 'https://github.com/taowuu/tao-router'
+    },
+    {
+      title: '一步步实现 Webpack',
+      url: 'https://github.com/taowuu/tao-webpack'
+    },
+    {
+      title: '一步步实现 Webpack loader',
+      url: 'https://github.com/taowuu/tao-loader'
+    },
+    {
+      title: '一步步实现 Webpack plugin',
+      url: 'https://github.com/taowuu/tao-plugin'
+    },
+    
   ]
 };
--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,7 +1,7 @@
 export function Blog(props) {
     return (
         <div>
-            <h3 class="text-2xl font-medium text-blue-500" style="padding-bottom: 10px">
+            <h3 class="text-2xl font-medium text-blue-500" style="padding-bottom: 13px">
                 <a href={props.url} target="_blank">{props.title}</a>
             </h3>
         </div>
