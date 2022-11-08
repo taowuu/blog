4c701492e55432a1ef3ca8a17ae42f4319a5f3bf
Taowuu <taowuu@foxmail.com>
2022-11-08 19:29:51 +0800

    add

--- meta.config.js
+++ meta.config.js
@@ -3,9 +3,9 @@ export default {
   skills: [
     'JavaScript',
     'TypeScript',
-    'Vue.js',
+    'Vue',
     'Webpack',
-    'Node.js',
+    'Node',
     'HTTP',
     'Algorithm',
     'HTML',
@@ -13,40 +13,60 @@ export default {
   ],
   blogs: [
     {
-      title: '前端知识沉淀',
-      url: 'https://github.com/taowuu/tao-blog'
+      title: 'Taowuu 的 JavaScript 全解',
+      url: 'https://github.com/taowuu/tao-javascript'
     },
     {
-      title: '前端手写大集合',
-      url: 'https://github.com/taowuu/tao-write'
+      title: 'Taowuu 的 TypeScript 全解',
+      url: 'https://github.com/taowuu/tao-typescript'
     },
     {
-      title: '前端布局大集合',
-      url: 'https://github.com/taowuu/tao-layout'
-    },
-    {
-      title: '前端面试攻略大全',
-      url: 'https://github.com/taowuu/fe-interview'
-    },
-    {
-      title: 'LeetCode JS 题解',
-      url: 'https://github.com/taowuu/tao-leetcode'
-    },
-    {
-      title: '一步步实现 Vue',
+      title: 'Taowuu 的 Vue 全解',
       url: 'https://github.com/taowuu/tao-vue'
     },
     {
-      title: '一步步实现 Vuex',
-      url: 'https://github.com/taowuu/tao-vuex'
-    },
-    {
-      title: '一步步实现 Vue Router',
-      url: 'https://github.com/taowuu/tao-router'
-    },
-    {
-      title: '一步步实现 Webpack',
+      title: 'Taowuu 的 Webpack 全解',
       url: 'https://github.com/taowuu/tao-webpack'
+    },
+    {
+      title: 'Taowuu 的 Node 全解',
+      url: 'https://github.com/taowuu/tao-node'
+    },
+    {
+      title: 'Taowuu 的 LeetCode 题解',
+      url: 'https://github.com/taowuu/tao-leetcode'
+    },
+    {
+      title: 'Taowuu 的前端随想录',
+      url: 'https://github.com/taowuu/tao-blog'
+    },
+    {
+      title: 'Taowuu 的前端手写大集合',
+      url: 'https://github.com/taowuu/tao-write'
+    },
+    {
+      title: 'Taowuu 的前端样式布局大集合',
+      url: 'https://github.com/taowuu/tao-layout'
+    },
+    {
+      title: 'Taowuu 的前端面试攻略',
+      url: 'https://github.com/taowuu/fe-interview'
+    },
+    {
+      title: '一步步实现 mini Vue',
+      url: 'https://github.com/taowuu/tao-mini-vue'
+    },
+    {
+      title: '一步步实现 mini Vuex',
+      url: 'https://github.com/taowuu/tao-mini-vuex'
+    },
+    {
+      title: '一步步实现 mini Vue Router',
+      url: 'https://github.com/taowuu/tao-mini-router'
+    },
+    {
+      title: '一步步实现 mini Webpack',
+      url: 'https://github.com/taowuu/tao-mini-webpack'
     },
     {
       title: '一步步实现 Webpack loader',
@@ -56,6 +76,5 @@ export default {
       title: '一步步实现 Webpack plugin',
       url: 'https://github.com/taowuu/tao-plugin'
     },
-    
   ]
 };
